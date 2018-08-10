43eb3805f3d8abc260ec145d08dd9f4c6799f32a
sujunfei <sujunfei@xiaomi.com>
2018-08-10 14:17:44 +0800

    solve the file name

--- commands/download.js
+++ commands/download.js
@@ -1,6 +1,8 @@
 const request = require('https').get
 const fs = require('fs')
 const path = require('path')
+const crypto = require('crypto')
+const md5 = crypto.createHash('md5')
 
 let source = ''
 let dest = ''
@@ -9,12 +11,12 @@ let fileName = ''
 const parseArgs = (args) => {
   let { url, dest, name } = args
   if (!url) {
-    console.error('[sd-error]: download file url must be given.')
+    console.error('\033[;33m [sd-error]: \033[;31m download file url must be given.')
     return
   }
 
   source = url
-  dest = path.join(process.env.PWD, dest)
+  dest = path.resolve(process.env.PWD, dest)
   if (name) {
     fileName = name
   }
@@ -24,9 +26,20 @@ const parseArgs = (args) => {
 
 const download = () => {
   request(source, (res) => {
-    let headers = res.headers
-    let contentType = headers['content-type']
+    let contentType = res.headers['content-type']
     let fileType = contentType.replace(/\w+\//, '')
+    if (!fileName) {
+      let matchReg = new RegExp('http(s)?:.+\.' + fileType, 'i')
+      let extractReg = /http(s)?:.+\/(?=(\w+)\.)/
+      if (matchReg.test(source)) {
+        fileName = source.replace(extractReg, (match, $1, $2) => {
+          return ''
+        })
+      } else {
+        md5.update(source)
+        fileName = 'sj-' + md5.digest('hex').slice(0, 7)
+      }
+    }
     let file = fs.createWriteStream(path.join(dest, fileName + '.' + fileType))
     res.pipe(file)
   })
